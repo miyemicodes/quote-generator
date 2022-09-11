c1124ff4f77ba08fbdced297fb755868e9730f64
miyemicodes <lizzymiyemi@gmail.com>
2022-09-11 23:35:16 +0100

    Quote

---
+++ index.js
@@ -0,0 +1,11 @@
+const quoteText = document.querySelector('.quote');
+quoteBtn = document.querySelector('button');
+/*
+function randomQuote(){
+   fetch("https://api.quotable.io.random").then(res => res.json()).then(result =>{});
+
+ }
+quoteBtn.addEventListener('click', randomQuote);
+*/
+
+//to be continued tommorow
